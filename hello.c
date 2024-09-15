b75068c5804eddcbef576b07ac06d1d6f6188fb2
psab2 <psabherwal2@huskers.unl.edu>
2024-09-14 22:13:51 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Prisha
+ * Date: 09/14/2024
+ *
+ * A simple code to print name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("prisha\n");
+
+    return 0;
+}
